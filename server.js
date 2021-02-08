60693e0837b13d17900d1eec7f5475e883559c67
Destiny Fleming <destiny.fleming7401@gmail.com>
2021-02-07 16:45:40 -0800

    started inquirer questions

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
-const mysql = require('mysql')
-const inquirer = require('inquirer')
 const console_table = require('console.table')
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',  
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'departments_db',
+  });
+  connection.connect( (err) => {
+      if (err) throw err;
+      askQuestions();
+  });
+  const askQuestions = () => {
+      inquirer.prompt({
+          name: 'action',
+          type: 'list',
+          message: 'What would you like to do?',
+          choices: [
+              'look at a department',
+              'create a department'
+          ]
+      }).then( (answer) => {
+          if (answer.action == 'look at a department') {
+              connection.query("SELECT * FROM departments", (err, res) => {
+                  if (err) throw err;
+                  console.log(res);
+              })
+          }
+      })
+  }
